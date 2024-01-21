b91d38bb949a9922528bbd55a7c693ca101dc583
isimmons <isimmons33@gmail.com>
2024-01-20 19:51:39 -0500

    build server heading and remove utility func

--- src/app/servers/[serverId]/page.tsx
+++ src/app/servers/[serverId]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
+import { CheckIcon, ChevronIcon, VerifiedIcon } from "~/components/Icons";
 import { servers, fakeChannels, fakeMessages } from "~/data";
-import { capitalize } from "~/utils";
+
 type Props = {
   params: { serverId: string };
 };
@@ -14,9 +15,14 @@ const ServerPage = ({ params }: Props) => {
   return (
     <>
       <div className="bg-gray-800 w-60 flex flex-col">
-        <div className="px-3 h-12 shadow-md flex items-center font-title text-white">
-          {capitalize(server.name)}
-        </div>
+        <button className="px-4 h-12 shadow-sm flex items-center font-title text-white text-[15px] hover:bg-gray-550/[0.16] transition">
+          <div className="relative size-4 mr-1">
+            <VerifiedIcon className="size-4 text-gray-550 absolute" />
+            <CheckIcon className="size-4 absolute" />
+          </div>
+          {server.name}
+          <ChevronIcon className="size-[18px] ml-auto opacity-80" />
+        </button>
         <div className="text-gray-300 font-medium flex-1 p-3 overflow-y-scroll scrollbar-fix space-y-2">
           <p className="text-white">channel (unread)</p>
           <p className="text-white">channel (unread)</p>
@@ -26,7 +32,7 @@ const ServerPage = ({ params }: Props) => {
         </div>
       </div>
       <div className="bg-gray-700 flex-1 flex flex-col">
-        <div className="h-12 px-3 shadow-md flex items-center font-title">
+        <div className="h-12 px-3 shadow-sm flex items-center font-title">
           General
         </div>
         <div className="flex-1 px-3 overflow-y-scroll space-y-4 scrollbar-fix">
